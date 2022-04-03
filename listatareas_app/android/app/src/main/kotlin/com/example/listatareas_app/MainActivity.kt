5c7c1a17d8d102046811da81480ba42669de8c1e
fidel palacios <herney400@gmail.com>
2022-04-02 23:19:44 -0500

    inicio curso

---
+++ listatareas_app/android/app/src/main/kotlin/com/example/listatareas_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listatareas_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ listatareas_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final items = List<String>.generate(20, (int i) => "Tarea ${i + 1}");
+  @override
+  Widget build(BuildContext context) {
+    final title = "Lista de tareas";
+    return MaterialApp(
+        title: title,
+        home: Scaffold(
+            appBar: AppBar(title: Text(title)),
+            body: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Dismissible(
+                    child: ListTile(title: Text('$item')),
+                    background: Container(color: Colors.green),
+                    key: Key(item),
+                    onDismissed: (direction) {
+                      items.removeAt(index);
+                    });
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text("$item eliminado")));
+              },
+            )));
+  }
+}
